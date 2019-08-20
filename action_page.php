0c8858e11a462fefcf3fbf53c825d39a9cb4c246
mthlmw <mthlmwtoo@gmail.com>
2019-08-20 12:28:38 -0400

    LDAP set up
    User authentication and session create, still need to iron out logging out of a session.

--- action_page.php
+++ action_page.php
@@ -1,27 +1,39 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-
 <?php
+session_start();
 
 $link = ldap_connect('metronihs.com'); // Your domain or domain server
 
 if(! $link) {
     // Could not connect to server - handle error appropriately
+	echo "failed link";
+	
 }
 
 ldap_set_option($link, LDAP_OPT_PROTOCOL_VERSION, 3); // Recommended for AD
 
 // Now try to authenticate with credentials provided by user
-if (! ldap_bind($link, 'ldap@metronihs.com', 'metron')) {
+if (! ldap_bind($link, $_POST["uname"]."@metronihs.com", $_POST["psw"])) {
     // Invalid credentials! Handle error appropriately
+	session_destroy();
+	echo "invalid credentials!";
+}
+
+else {
+	//echo "Hello" . htmlspecialchars($_POST["uname"]) . '!';
+	  header("Location: create.php");
+
 }
 
 ?>
 
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+
+
 </head>
 <body>
-Hello World!
+Something Went Wrong!
 </body>
 </html>
