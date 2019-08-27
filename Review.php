ed91963117ba04d6e66ed881afded8598eb01cee
jasongunderson <jasongunderson@live.com>
2019-08-27 13:16:35 -0400

    testing review sql query

--- Review.php
+++ Review.php
@@ -29,11 +29,44 @@ include 'session_check.php'
 	</div>
 	<div>
 		<?php
-		echo $_SESSION["user"]."<br>";
-		echo $_SESSION["facility"]."<br>";
-		echo $_SESSION["department"]."<br>";
-		?>
+		$connString = "mysql:host=localhost;dbname=metronpo";
+        $username="metronpo";
+        $password="N0tTh3P@ssword";
+
+        $value=$_SESSION["facility"];
+        $pdo= new PDO($connString,$username,$password);
+        $sql = "SELECT uid,facility,cost,date_submit FROM requests WHERE facility = ?";
+        $statement = $pdo->prepare($sql);
+        $statement->bindValue(1, $value);
+        $statement->execute();
+                
+        $pdo = null;
+
+        if($statement->rowCount() == 0)
+        {
+          echo "<h2>No Results Found</h2>\n";
+        }
+        else{
+		echo "<form role=\"form\" class=\"requests_form_class\" action=\"\" id=\"request_form_id\">";
+            echo "<table id=\"requests_table_id\" class=\"requests_table_class\">\n";
+            echo "<th class=\"tableheader\">Name</th><th class=\"tableheader\">Facility</th><th class=\"tableheader\">Cost</th><th class=\"tableheader\">Date</th>\n";
+          $result =  $statement;
+             foreach($result as $row){
+              echo "<tr class=\"tablerow\"><td>".$row[0]."</td><td>".$row[1]."</td><td>".$row[2]."</td><td>".$row[3]."</td></tr>\n";
+              
+             }
+             echo "</table>";
+             echo "</form>";
+        } //end else
+             ?>
 	</div>
+<!-- 	<div>
+		<?php
+		// echo $_SESSION["user"]."<br>";
+		// echo $_SESSION["facility"]."<br>";
+		// echo $_SESSION["department"]."<br>";
+		?>
+	</div> -->
 </div>
 </body>
 
