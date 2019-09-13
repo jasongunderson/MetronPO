67b57790b2091ab1be315f8ed5655fca96e1fcd3
jasongunderson <jasongunderson@live.com>
2019-09-13 15:11:07 -0400

    testing

--- addPO.php
+++ addPO.php
@@ -2,8 +2,8 @@
 session_start();
 
 // ** If not logged in reirect to login page
-if(!isset($_SESSION["user"])){
-  header("Location: metronpo.php");
+// if(!isset($_SESSION["user"])){
+//   header("Location: metronpo.php");
 
 //verify inputs
 
